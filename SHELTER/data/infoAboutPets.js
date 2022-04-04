7c2cc94f28c8de3b322e602d3202db801afe122b
Zaiko Euheni <rahunak@yandex.by>
2022-04-04 21:15:43 +0300

    i'm very tired

--- SHELTER/data/infoAboutPets.js
+++ SHELTER/data/infoAboutPets.js
@@ -3,7 +3,7 @@ let info=[{
     h3:"Charly",
     h4:"Dog - Jack Russell Terrier ",
     h5:"This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
-    img:"/assets/images/pets-charly.png",
+    img:"/SHELTER/assets/images/pets-charly.png",
     alt:"dog Charly",
 },{
     h3:"Freddie",
