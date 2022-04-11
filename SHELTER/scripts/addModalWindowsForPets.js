1240ae6c2cfdb6d1572f2512128251971ce14c25
Zaiko Euheni <rahunak@yandex.by>
2022-04-11 11:00:47 +0300

    fix: did a little staff

--- SHELTER/scripts/addModalWindowsForPets.js
+++ SHELTER/scripts/addModalWindowsForPets.js
@@ -10,7 +10,7 @@ document.querySelectorAll(".card__button").forEach(el => el.addEventListener("cl
 
 function showModalWindow(event) {
     let name = event.currentTarget.previousElementSibling.textContent.toUpperCase();
-    let petObj = info.find(el => el.h3.toUpperCase() === name);
+    let petObj = info.find(el => el.name.toUpperCase() === name);
     let modal = document.createElement("div");
     modal.style.top = window.pageYOffset + "px";
     modal.style.left = 0 + "px";
@@ -19,22 +19,22 @@ function showModalWindow(event) {
 <div class="wrapper modalWindow__wrapper">
     <img src="${petObj.img}" alt="${petObj.alt}" class="modalWindow__img"/>
     <div class="modalWindow__description">
-        <h3 class="modalWindow__h3">${petObj.h3}</h3>
-        <h4 class="modalWindow__h4">${petObj.h4}</h4>
-        <h5 class="modalWindow__h5">${petObj.h5}</h5>
+        <h3 class="modalWindow__h3">${petObj.name}</h3>
+        <h4 class="modalWindow__h4">${petObj.type} - ${petObj.breed}</h4>
+        <h5 class="modalWindow__h5">${petObj.description}</h5>
         
         <ul  class="modalWindow__ul">
             <li class="modalWindow__li">
-                <h5 class="modalWindow__h5_bold">Age:<span> 2 months</span></h5>
+                <h5 class="modalWindow__h5_bold">Age:<span>${petObj.age}</span></h5>
             </li>
             <li class="modalWindow__li">
-                <h5 class="modalWindow__h5_bold">Inoculations:<span> none</span></h5>
+                <h5 class="modalWindow__h5_bold">Inoculations:<span> ${petObj.inoculations}</span></h5>
             </li>
             <li class="modalWindow__li">
-                <h5 class="modalWindow__h5_bold">Diseases:<span> none</span></h5>
+                <h5 class="modalWindow__h5_bold">Diseases:<span> ${petObj.diseases}</span></h5>
             </li>
             <li class="modalWindow__li">
-                <h5 class="modalWindow__h5_bold">Parasites:<span> none</span></h5>
+                <h5 class="modalWindow__h5_bold">Parasites:<span> ${petObj.parasites}</span></h5>
             </li>
         </ul>
     </div>
@@ -56,6 +56,7 @@ function hideModalWindow() {
 function clickLACK(event) {
     console.log("mouseClick ", event.clientX, "Y", event.clientY)
     let modal= document.querySelector(".modalWindow__wrapper");
+    if(!modal)return;
     let coordsModal=modal.getBoundingClientRect();
     console.log("coords",coordsModal)
     if(event.clientX<coordsModal.left || event.clientX>coordsModal.right 
