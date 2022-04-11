04bc8f23d1e23dc11976732fcc69b22663ce2afb
Zaiko Euheni <rahunak@yandex.by>
2022-04-11 13:31:13 +0300

    fix: correct adreses(

--- SHELTER/scripts/addModalWindowsForPets.js
+++ SHELTER/scripts/addModalWindowsForPets.js
@@ -42,7 +42,7 @@ function showModalWindow(event) {
 </div>
 
 <button class="modalWindow__btn_close">
-    <img src="/assets/icons/close-button.svg" alt="press to close">
+    <img src="/SHELTER/assets/icons/close-button.svg" alt="press to close">
 </button>
 </div>
 `);
